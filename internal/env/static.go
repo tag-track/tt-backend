967788db26db9d03c9c41c2f51c6bfc3facfe27d
Tsien Jin <46650957+TsienJin@users.noreply.github.com>
2025-01-24 01:48:49 +0800

    Updated env variables to invlude minio

--- internal/env/static.go
+++ internal/env/static.go
@@ -5,11 +5,16 @@ import "github.com/caarlos0/env/v11"
 type StaticEnvStruct struct {
 	ServerPort int `env:"SERVER_PORT"`
 
-	DbHost     string `env:"DB_HOST"`
-	DbPort     int    `env:"DB_PORT"`
-	DbUser     string `env:"DB_USER"`
-	DbPassword string `env:"DB_PASSWORD"`
-	DbName     string `env:"DB_NAME"`
+	DbHost        string `env:"DB_HOST"`
+	DbPort        int    `env:"DB_PORT"`
+	DbUser        string `env:"DB_USER"`
+	DbPassword    string `env:"DB_PASSWORD"`
+	DbName        string `env:"DB_NAME"`
+	MinioHost     string `env:"MINIO_HOST"`
+	MinioPort     string `env:"MINIO_PORT_API"`
+	MinioUser     string `env:"MINIO_USER"`
+	MinioPassword string `env:"MINIO_PASSWORD"`
+	MinioBucket   string `env:"MINIO_BUCKET"`
 }
 
 var (
